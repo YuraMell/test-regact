aa2115e5db7a9e99e0201d96093b416f9f304ebf
YuraMell <yura.melnik7523@gmail.com>
2022-01-05 16:23:20 +0200

    Change header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,46 +3,46 @@ import logo from '../images/logo.png'
 
 const Header = () => {
   return (
-		<>
-			<div className="burger-menu">
-	  		<button className="burger-menu_button">
-	    <span className="burger-menu_lines"></span>
-	  </button>
-	  		<nav className="burger-menu_nav">
-				<ul>
-					<li className="burger-menu_link"><a href="#">Для мужчин</a></li>
-					<li className="burger-menu_link"><a href="#">Для женщин</a></li>
-					<li className="burger-menu_link"><a href="#">Для детей</a></li>
-					<li className="logo burger-menu_link">
-						<img src={logo} alt="" />
-					</li>
-					<li className="burger-menu_link"><a href="#">Оплата</a></li>
-					<li className="burger-menu_link"><a href="#">Доставка</a></li>
-				</ul>
-	  		</nav>
-	  		<div className="burger-menu_overlay" />
-			</div>
+    <>
+      <div className="burger-menu">
+        <button className="burger-menu_button">
+          <span className="burger-menu_lines"></span>
+        </button>
+        <nav className="burger-menu_nav">
+          <ul>
+            <li className="burger-menu_link"><a href="#">Для мужчин</a></li>
+            <li className="burger-menu_link"><a href="#">Для женщин</a></li>
+            <li className="burger-menu_link"><a href="#">Для детей</a></li>
+            <li className="logo burger-menu_link">
+              <img src={logo} alt="" />
+            </li>
+            <li className="burger-menu_link"><a href="#">Оплата</a></li>
+            <li className="burger-menu_link"><a href="#">Доставка</a></li>
+          </ul>
+        </nav>
+        <div className="burger-menu_overlay" />
+      </div>
 
       <section className="head">
-				<div className="inner">
-					<header>
-						<ul>
-							<li><a href="#">Для мужчин</a></li>
-							<li><a href="#">Для женщин</a></li>
-							<li><a href="#">Для детей</a></li>
-							<li className="logo">
-								<img src="../images/logo.png" alt="" />
-							</li>
-							<li><a href="#">Оплата</a></li>
-							<li><a href="#">Доставка</a></li>
-						</ul>
-						<div className="cart">
-							<img src="../images/cart.svg" alt="" />
-							<strong>5</strong>
-						</div>
-					</header>
-		</div>
-			</section>
+        <div className="inner">
+          <header>
+            <ul>
+              <li><a href="#">Для мужчин</a></li>
+              <li><a href="#">Для женщин</a></li>
+              <li><a href="#">Для детей</a></li>
+              <li className="logo">
+                <img src="../images/logo.png" alt="" />
+              </li>
+              <li><a href="#">Оплата</a></li>
+              <li><a href="#">Доставка</a></li>
+            </ul>
+            <div className="cart">
+              <img src="../images/cart.svg" alt="" />
+              <strong>5</strong>
+            </div>
+          </header>
+        </div>
+      </section>
     </>
   )
 }
