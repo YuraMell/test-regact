244026ff7fc007a7a760bc3127e55a31dda0c22c
prusichka <denchuk.prusak@gmail.com>
2022-01-05 17:06:48 +0200

    exp

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,12 @@
 import React from 'react'
 import logo from '../images/logo.png'
 import cart from '../images/cart.svg'
+import MobileMenu from "./mobileMenu";
 
 const Header = () => {
   return (
 		<>
-			<div className="burger-menu">
-	  		<button className="burger-menu_button">
-	    <span className="burger-menu_lines"/>
-	  </button>
-	  		<nav className="burger-menu_nav">
-				<ul>
-					<li className="burger-menu_link"><a href="#">Для мужчин</a></li>
-					<li className="burger-menu_link"><a href="#">Для женщин</a></li>
-					<li className="burger-menu_link"><a href="#">Для детей</a></li>
-					<li className="logo burger-menu_link">
-						<img src={logo} alt="" />
-					</li>
-					<li className="burger-menu_link"><a href="#">Оплата</a></li>
-					<li className="burger-menu_link"><a href="#">Доставка</a></li>
-				</ul>
-	  		</nav>
-	  		<div className="burger-menu_overlay" />
-			</div>
-
+			<MobileMenu/>
       <section className="head">
 				<div className="inner">
 					<header>
