564a492f4ccc628fd7328b084f1ddd8f1b2a0f6f
prusichka <denchuk.prusak@gmail.com>
2022-01-05 16:57:06 +0200

    add mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,47 +1,49 @@
 import React from 'react'
 import logo from '../images/logo.png'
 import cart from '../images/cart.svg'
-import { Link } from 'react-router-dom'
-
 
 const Header = () => {
   return (
-    <>
-      <div className="burger-menu">
-        <button className="burger-menu_button">
-          <span className="burger-menu_lines"></span>
-        </button>
-        <nav className="burger-menu_nav">
-          <ul>
-            <li className="burger-menu_link"><a href="#">Для мужчин</a></li>
-            <li className="burger-menu_link"><a href="#">Для женщин</a></li>
-            <li className="burger-menu_link"><a href="#">Для детей</a></li>
-            <li><Link to='/' className="logo burger-menu_link"><img src={logo} alt="" /></Link></li>
-            <li><Link className="burger-menu_link" to='/about'>Оплата</Link></li>
-            <li><Link className="burger-menu_link" to="/contacts">Доставка</Link></li>
-          </ul>
-        </nav>
-        <div className="burger-menu_overlay" />
-      </div>
+		<>
+			<div className="burger-menu">
+	  		<button className="burger-menu_button">
+	    <span className="burger-menu_lines"/>
+	  </button>
+	  		<nav className="burger-menu_nav">
+				<ul>
+					<li className="burger-menu_link"><a href="#">Для мужчин</a></li>
+					<li className="burger-menu_link"><a href="#">Для женщин</a></li>
+					<li className="burger-menu_link"><a href="#">Для детей</a></li>
+					<li className="logo burger-menu_link">
+						<img src={logo} alt="" />
+					</li>
+					<li className="burger-menu_link"><a href="#">Оплата</a></li>
+					<li className="burger-menu_link"><a href="#">Доставка</a></li>
+				</ul>
+	  		</nav>
+	  		<div className="burger-menu_overlay" />
+			</div>
 
       <section className="head">
-        <div className="inner">
-          <header>
-            <ul>
-              <li><a href="#">Для мужчин</a></li>
-              <li><a href="#">Для женщин</a></li>
-              <li><a href="#">Для детей</a></li>
-              <li><Link to='/' className="logo burger-menu_link"><img src={logo} alt="" /></Link></li>
-              <li><Link className="burger-menu_link" to='/about'>Оплата</Link></li>
-              <li><Link className="burger-menu_link" to="/contacts">Доставка</Link></li>
-            </ul>
-            <div className="cart">
-              <img src={cart} alt="" />
-              <strong>5</strong>
-            </div>
-          </header>
-        </div>
-      </section>
+				<div className="inner">
+					<header>
+						<ul>
+							<li><a href="#">Для мужчин</a></li>
+							<li><a href="#">Для женщин</a></li>
+							<li><a href="#">Для детей</a></li>
+							<li className="logo">
+									<img src={logo} alt='zxc'/>
+							</li>
+							<li><a href="#">Оплата</a></li>
+							<li><a href="#">Доставка</a></li>
+						</ul>
+						<div className="cart">
+							<img src={cart} alt="" />
+							<strong>5</strong>
+						</div>
+					</header>
+		</div>
+			</section>
     </>
   )
 }
