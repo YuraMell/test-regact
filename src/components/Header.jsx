2ca8519f408b6dcd3dcb5f6825b10583344c10ae
YuraMell <yura.melnik7523@gmail.com>
2022-01-05 17:47:26 +0200

    change navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import logo from '../images/logo.png'
 import cart from '../images/cart.svg'
 import MobileMenu from "./mobileMenu";
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
@@ -14,11 +15,9 @@ const Header = () => {
               <li><a href="#">Для мужчин</a></li>
               <li><a href="#">Для женщин</a></li>
               <li><a href="#">Для детей</a></li>
-              <li className="logo">
-                <img src={logo} alt='logo' />
-              </li>
-              <li><a href="#">Оплата</a></li>
-              <li><a href="#">Доставка</a></li>
+              <Link to='/' className="logo"><img src={logo} alt='logo' /></Link>
+              <Link to='/about'>Оплата</Link>
+              <Link to='/contacts'>Доставка</Link>
             </ul>
             <div className="cart">
               <img src={cart} alt="" />
