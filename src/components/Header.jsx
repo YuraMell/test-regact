1527935aa187bfc8f9fc1adf691777c64b0878ab
YuraMell <yura.melnik7523@gmail.com>
2022-01-05 17:42:28 +0200

    Change alt

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,28 +5,28 @@ import MobileMenu from "./mobileMenu";
 
 const Header = () => {
   return (
-		<>
-			<MobileMenu/>
+    <>
+      <MobileMenu />
       <section className="head">
-				<div className="inner">
-					<header>
-						<ul>
-							<li><a href="#">Для мужчин</a></li>
-							<li><a href="#">Для женщин</a></li>
-							<li><a href="#">Для детей</a></li>
-							<li className="logo">
-									<img src={logo} alt='zxc'/>
-							</li>
-							<li><a href="#">Оплата</a></li>
-							<li><a href="#">Доставка</a></li>
-						</ul>
-						<div className="cart">
-							<img src={cart} alt="" />
-							<strong>5</strong>
-						</div>
-					</header>
-		</div>
-			</section>
+        <div className="inner">
+          <header>
+            <ul>
+              <li><a href="#">Для мужчин</a></li>
+              <li><a href="#">Для женщин</a></li>
+              <li><a href="#">Для детей</a></li>
+              <li className="logo">
+                <img src={logo} alt='logo' />
+              </li>
+              <li><a href="#">Оплата</a></li>
+              <li><a href="#">Доставка</a></li>
+            </ul>
+            <div className="cart">
+              <img src={cart} alt="" />
+              <strong>5</strong>
+            </div>
+          </header>
+        </div>
+      </section>
     </>
   )
 }
